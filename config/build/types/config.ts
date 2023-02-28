0d0f7ee01c9f655a12533f3dd5e07564f427b1df
Michael Chigorin <justamzingemail@gmail.com>
2023-02-28 22:23:33 +0300

    configure env, production and dev builds

--- config/build/types/config.ts
+++ config/build/types/config.ts
@@ -1,5 +1,10 @@
 export type BuildModeType = "production" | "development";
 
+export interface BuildEnv {
+  mode: BuildModeType;
+  port: number;
+}
+
 export interface BuildPaths {
   entry: string;
   build: string;
--- webpack.config.ts
+++ webpack.config.ts
@@ -1,23 +1,29 @@
 import webpack from "webpack";
 import path from "path";
 import buildWebpackConfig from "./config/build/build.webpack.config";
-import { BuildModeType, BuildPaths } from "./config/build/types/config";
+import {
+  BuildEnv,
+  BuildModeType,
+  BuildPaths,
+} from "./config/build/types/config";
 
-const paths: BuildPaths = {
-  entry: path.resolve(__dirname, "src", "index.ts"),
-  build: path.resolve(__dirname, "build"),
-  html: path.resolve(__dirname, "public", "index.html"),
+export default (env: BuildEnv) => {
+  const paths: BuildPaths = {
+    entry: path.resolve(__dirname, "src", "index.ts"),
+    build: path.resolve(__dirname, "build"),
+    html: path.resolve(__dirname, "public", "index.html"),
+  };
+
+  const mode: BuildModeType = env.mode || "development";
+  const isDev = env.mode === "development";
+  const PORT = env.port || 3000;
+
+  const config: webpack.Configuration = buildWebpackConfig({
+    mode,
+    paths,
+    isDev,
+    port: PORT,
+  });
+
+  return config;
 };
-
-const mode: BuildModeType = "development";
-const isDev = mode === "development";
-const PORT = 3000;
-
-const config: webpack.Configuration = buildWebpackConfig({
-  mode,
-  paths,
-  isDev,
-  port: PORT,
-});
-
-export default config;
